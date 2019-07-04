af070943b97d7d70c404f7badabf546ae842852a
Midhet Sulemani <m.sulemani@yabila.com>
2019-07-04 09:48:54 +0300

    Initial Commit

---
+++ ViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewTest
+//
+//  Created by Waveline Media on 7/4/19.
+//  Copyright © 2019 Waveline Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
